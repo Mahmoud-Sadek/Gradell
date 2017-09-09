de59642078e6739f0fd6364287aa4a7f9f28e72f
Mahmoud-Sadek <mahmoud.sadek.96@gmail.com>
2017-09-09 15:18:16 +0200

    firstCommit

---
+++ app/src/free/java/com/example/amiraahabeeb/flavors/free/MainActivityFragment.java
@@ -0,0 +1,60 @@
+package com.example.amiraahabeeb.flavors.free;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.example.amiraahabeeb.jokeactivity.JokeActivity;
+import com.example.amiraahabeeb.joketelling.EndpointsAsyncTask;
+import com.example.amiraahabeeb.joketelling.OnTaskCompleted;
+import com.example.amiraahabeeb.joketelling.R;
+
+
+public class MainActivityFragment extends Fragment implements OnTaskCompleted {
+    Button mJokeButton;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        mJokeButton = (Button) root.findViewById(R.id.btn_joke);
+
+        mJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData();
+            }
+        });
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+        return root;
+    }
+    public void loadData() {
+//        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(this);
+//        endpointsAsyncTask.execute();
+    }
+    @Override
+    public void onTaskCompleted(String result) {
+        Intent intent = new Intent(getActivity(), JokeActivity.class);
+
+        intent.putExtra(JokeActivity.JOKE_KEY, result);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/amiraahabeeb/joketelling/OnTaskCompleted.java
@@ -0,0 +1,9 @@
+package com.example.amiraahabeeb.joketelling;
+
+/**
+ * Created by amiraahabeeb on 5/9/2017.
+ */
+
+public interface OnTaskCompleted {
+    void onTaskCompleted(String result);
+}
